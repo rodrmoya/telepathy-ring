a5afd901fdc6fd46fa92c35c225f3b1fc558a8df
Rodrigo Moya <rodrigo.moya@collabora.co.uk>
2013-08-06 15:53:44 +0200

    Use RingBaseCallChannel as prerequisite for RingMemberChannel interface

--- src/ring-member-channel.c
+++ src/ring-member-channel.c
@@ -22,7 +22,7 @@
 #include "config.h"
 
 #include "ring-member-channel.h"
-#include "ring-media-channel.h"
+#include "base-call-channel.h"
 
 #include <telepathy-glib/gtypes.h>
 #include <telepathy-glib/util.h>
@@ -79,7 +79,7 @@ ring_member_channel_get_type(void)
     type = g_type_register_static(
       G_TYPE_INTERFACE, "RingMemberChannel", &info, 0);
 
-    g_type_interface_add_prerequisite(type, RING_TYPE_MEDIA_CHANNEL);
+    g_type_interface_add_prerequisite(type, RING_TYPE_BASE_CALL_CHANNEL);
   }
 
   return type;
